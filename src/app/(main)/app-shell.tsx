cf0cfe5b785f4c1161a326025ae5d5d94485e9b5
Haxis Woins <2651550221@qq.com>
2025-08-25 07:55:45 +0000

    为我删除这一效果

--- src/app/(main)/app-shell.tsx
+++ src/app/(main)/app-shell.tsx
@@ -18,7 +18,6 @@ import Image from 'next/image';
 import type { SiteContent } from '@/types';
 import { motion } from 'framer-motion';
 import { cn } from '@/lib/utils';
-import { AestheticFluidBackground } from '@/components/aesthetic-fluid-background';
 
 
 function MainContentWrapper({
@@ -36,7 +35,6 @@ function MainContentWrapper({
     <>
       {/* Background Effects Layer */}
       <div className="fixed inset-0 z-0 overflow-hidden">
-         {isHomePage && <AestheticFluidBackground />}
         {hasHomeBg && (
           <div className="absolute inset-0 z-5">
               <Image
