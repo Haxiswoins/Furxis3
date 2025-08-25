05eca110894e6a30675e688f89f4334a1f00c5dc
Haxis Woins <2651550221@qq.com>
2025-08-25 04:43:41 +0000

    算了，这个效果也删掉吧

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 'use client';
 
-import { LandingPageClient } from '@/components/landing-client';
+import { getSiteContent } from '@/lib/data-service';
+import { HomeClient } from '@/components/home-client';
+import type { SiteContent } from '@/types';
+import { useEffect, useState } from 'react';
 
+// This is now a Server Component
 export default function Home() {
+  const [content, setContent] = useState<SiteContent | null>(null);
+
+  useEffect(() => {
+    getSiteContent().then(setContent);
+  }, []);
+
+
   return (
-    <>
-      <LandingPageClient />
-    </>
+    <div className="container mx-auto">
+        <HomeClient content={content} />
+    </div>
   );
 }
