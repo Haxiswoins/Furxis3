6b68e251518e86b26484b123bc8043175f014d30
Haxis Woins <2651550221@qq.com>
2025-08-26 02:50:46 +0000

    网站内容依旧消失不见，你不是有个文件会记录你所做的修改的吗？你参考那个文件给我进行回退啊，为什么会删错东西？

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn(
-        "font-body antialiased loading-initial",
+        "font-body antialiased",
         fontHeadline.variable,
         fontSerifSC.variable,
         fontBody.variable
