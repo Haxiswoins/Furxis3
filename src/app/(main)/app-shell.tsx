39a8f85457a8b7836a1d3a7f63f98588d4e1fc64
Haxis Woins <2651550221@qq.com>
2025-08-26 13:38:19 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <HomeClient conten

--- src/app/(main)/app-shell.tsx
+++ src/app/(main)/app-shell.tsx
@@ -27,7 +27,7 @@ function MainContentWrapper({
     <>
       <Header />
       <div className="relative z-10 flex flex-col min-h-screen bg-transparent">
-        <main className="flex-1 flex flex-col items-center justify-center">
+        <main className="flex-1 flex flex-col justify-center">
           {children}
         </main>
       </div>
