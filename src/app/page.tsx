02e6755b85b3e917d0bae2ddd39a8247618017d7
Haxis Woins <2651550221@qq.com>
2025-08-26 07:25:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-
 'use client';
 
 import { Suspense } from 'react';
-import { LandingPageClient } from '../landing-client';
+import { LandingPageClient } from '@/components/landing-client';
 
 export default function WelcomePage() {
   return (
