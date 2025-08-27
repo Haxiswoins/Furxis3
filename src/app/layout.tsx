d592d625532476dfb932a1001969aff0d83d6d69
Haxis Woins <2651550221@qq.com>
2025-08-27 06:42:32 +0000

    但是当这一背景动效加载出来后，网站变得略卡，有无优化的办法？

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
             <Toaster />
           </ThemeProvider>
           {/* The script is now loaded here in the root layout to be available globally */}
-          <Script src="/AmbientLightBg.min.js" strategy="beforeInteractive" />
+          <Script src="/AmbientLightBg.min.js" strategy="lazyOnload" />
       </body>
     </html>
   );
