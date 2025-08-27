84246b8bdf731268f584191ccb2dc801a0026a3e
Haxis Woins <2651550221@qq.com>
2025-08-27 06:29:07 +0000

    现在我的网站的动画效果是完整且好看的，我希望你为我保持。接下来请你为我全面检查一下这个网站有无任何BUG，尤其是页面切换，欢迎页与首页的要素是否

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@
 import { useRouter } from 'next/navigation';
 import { FluidBackground } from '@/components/fluid-background';
 import { LandingPageClient } from '@/components/landing-client';
-import Header from '@/components/header';
 
 export default function WelcomePage() {
     const router = useRouter();
@@ -16,7 +15,6 @@ export default function WelcomePage() {
     return (
         <div className="relative min-h-screen w-full bg-background">
             <FluidBackground />
-            <Header />
             <LandingPageClient onNavigate={handleNavigate} />
         </div>
     );
