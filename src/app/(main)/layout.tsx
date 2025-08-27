74aab188f6323e67a450ae6ae53f3f0b9063d666
Haxis Woins <2651550221@qq.com>
2025-08-27 09:05:32 +0000

    我并没有同意这一方案，我否决它

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -11,8 +11,9 @@ export default async function MainLayout({
   // Data is fetched on the server
   const siteContent = await getSiteContent();
 
+  // siteContent is passed down as a prop
+  // Removed the extra div wrapper that was causing a black flash on page transitions.
   return (
-    // siteContent is passed down as a prop
     <AppShell siteContent={siteContent}>
         {children}
     </AppShell>
