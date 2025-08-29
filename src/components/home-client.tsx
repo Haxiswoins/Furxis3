c34f7f86c94689c20909731a662c525362db2ea4
Haxis Woins <2651550221@qq.com>
2025-08-29 08:46:14 +0000

    为我把主页背景去掉吧

--- src/components/home-client.tsx
+++ src/components/home-client.tsx
@@ -58,20 +58,6 @@ export function HomeClient({ content }: HomeClientProps) {
 
   return (
     <>
-       {content?.homeBackgroundImageUrl && (
-        <>
-            <div className="fixed inset-0 z-[-1]">
-            <Image
-                src={content.homeBackgroundImageUrl}
-                alt="主页背景"
-                fill
-                style={{ objectFit: 'cover' }}
-                className="opacity-90"
-            />
-             <div className="absolute inset-0 bg-black/30"></div>
-            </div>
-        </>
-      )}
       <div className="container mx-auto">
         <motion.div 
             className="relative z-10 flex flex-col min-h-screen"
