26bf9210885e181f23c866d106a6acf64fce1c25
Haxis Woins <2651550221@qq.com>
2025-09-09 09:06:23 +0000

    遮罩似乎消失了，要不这样吧，为我把委托申请、设定领养、作品一览三个标题增加一些阴影，这样也能在我添加背景图片后突出文字的显示，我们不一定要用遮罩

--- Users/jovyan/workspace/src/tailwind.config.ts
+++ Users/jovyan/workspace/src/tailwind.config.ts
@@ -112,7 +112,7 @@ const config: Config = {
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-cover-bottom': 'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.1) 30%, transparent 40%)',
+        'gradient-cover-bottom': 'linear-gradient(to top, hsla(var(--card-foreground) / 0.7) 8%, hsla(var(--card-foreground) / 0.1) 30%, transparent 40%)',
       }
     },
   },
