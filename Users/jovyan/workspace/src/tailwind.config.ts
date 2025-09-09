250ef80ffe70708bce2fe216815dcbee1b72a776
Haxis Woins <2651550221@qq.com>
2025-09-09 08:50:39 +0000

    你确定修改奏效了吗

--- Users/jovyan/workspace/src/tailwind.config.ts
+++ Users/jovyan/workspace/src/tailwind.config.ts
@@ -112,7 +112,7 @@ const config: Config = {
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-cover-bottom': 'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 30%, transparent 50%)',
+        'gradient-cover-bottom': 'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 30%, transparent 40%)',
       }
     },
   },
