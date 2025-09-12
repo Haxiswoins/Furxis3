1c9d8fa19b8a1347649042e1995e9a9474ff0fe0
Haxis Woins <2651550221@qq.com>
2025-09-12 07:52:32 +0000

    出现了如下错误：
    root@VM-8-11-ubuntu:~/Furxis3# npm run build
    
    > nextn@0.1.0 bu

--- Users/jovyan/workspace/src/app/(main)/admin/users/page.tsx
+++ Users/jovyan/workspace/src/app/(main)/admin/users/page.tsx
@@ -1,14 +1,15 @@
-
 import { getAggregatedUsers, getBadges } from '@/lib/data-service';
 import { UserManagementPageClient } from './client-page';
 
 export const dynamic = 'force-dynamic';
 
 export default async function UserManagementPage() {
-  const [userData, badgeData] = await Promise.all([
+  // This is a server component, responsible for fetching initial data.
+  const [users, badges] = await Promise.all([
     getAggregatedUsers(),
     getBadges(),
   ]);
 
-  return <UserManagementPageClient users={userData} badges={badgeData} />;
+  // The client component receives the data as props to handle interactivity.
+  return <UserManagementPageClient initialUsers={users} initialBadges={badges} />;
 }
