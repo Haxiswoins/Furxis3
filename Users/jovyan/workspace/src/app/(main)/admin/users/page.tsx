088e9b726a7196a15e95980aaf0178bca868c3ca
Haxis Woins <2651550221@qq.com>
2025-09-17 01:56:36 +0000

    Linting and checking validity of types  .Failed to compile.
    ./Users/jov

--- Users/jovyan/workspace/src/app/(main)/admin/users/page.tsx
+++ Users/jovyan/workspace/src/app/(main)/admin/users/page.tsx
@@ -1,4 +1,5 @@
 
+import * as React from 'react';
 import { getAggregatedUsers, getBadges } from '@/lib/data-service';
 import { UserManagementPageClient } from './client-page';
 import { Skeleton } from '@/components/ui/skeleton';
