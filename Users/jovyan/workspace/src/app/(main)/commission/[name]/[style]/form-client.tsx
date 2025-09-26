ab99c27fb27d6b662f4fb4c1ccfd0cd09d28a39c
Haxis Woins <2651550221@qq.com>
2025-09-26 09:56:10 +0000

    Linting and checking validity of types  ...Failed to compile.
    ./src/app

--- Users/jovyan/workspace/src/app/(main)/commission/[name]/[style]/form-client.tsx
+++ Users/jovyan/workspace/src/app/(main)/commission/[name]/[style]/form-client.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
