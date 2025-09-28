290cbb8e51f27d2479b1dd20e68f71df88534654
Haxis Woins <2651550221@qq.com>
2025-09-28 07:35:38 +0000

    Linting and checking validity of types  ..Failed to compile.
    ./src/app/

--- Users/jovyan/workspace/src/app/(main)/commission/[name]/[style]/form-client.tsx
+++ Users/jovyan/workspace/src/app/(main)/commission/[name]/[style]/form-client.tsx
@@ -195,7 +195,7 @@ export function CommissionApplicationFormClient({ commissionOption, commissionSt
       const commissionInfo = {
         styleName: commissionStyle.name,
         optionName: commissionOption.name,
-        imageUrl: commissionStyle.imageUrl,
+        imageUrl: commissionStyle.imageUrl || '',
         price: commissionStyle.price,
       };
 
