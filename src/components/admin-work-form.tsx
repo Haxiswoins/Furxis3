bde8a6f0b73ad7b8329b42dbe49a5b9da30ce296
Haxis Woins <2651550221@qq.com>
2025-09-29 01:50:55 +0000

    Changes before Firebase Studio auto-run

--- src/components/admin-work-form.tsx
+++ src/components/admin-work-form.tsx
@@ -1,6 +1,5 @@
 
-'use client';
-
+import * as React from 'react';
 import { useState, useRef, useMemo } from 'react';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
